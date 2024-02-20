f2d8a28921391a485114d87530f494aa2ef0252d
dismal <118924562+Dismalitie@users.noreply.github.com>
2024-02-20 20:13:02 +0000

    v1.1

--- inui/Form1.cs
+++ inui/Form1.cs
@@ -38,24 +38,44 @@ namespace inui
             process.StartInfo = processStartInfo;
             process.Start();
         }
+        void runLua(string file)
+        {
+            string command = Environment.CurrentDirectory + "\\lua\\lua.exe " + file;
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.FileName = "cmd.exe";
+            processStartInfo.Arguments = "/c " + command;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.UseShellExecute = false;
+            Process process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+        }
+        void runAll(string lua, string cmd) // func nesting or smh lol
+        {
+            runCmd(cmd);
+            runLua(lua);
+        }
         void runCheckBox(bool state, IniFile file, string element)
         {
-            runCmd(Environment.CurrentDirectory + "\\lua.exe " + file.GetSetting(element, "onClicked.lua", ""));
+            runAll(file.GetSetting(element, "onClicked.lua", ""), file.GetSetting(element, "onClicked.cmd", ""));
             if (state)
             {
-                runCmd(Environment.CurrentDirectory + "\\lua.exe " + file.GetSetting(element, "onChecked.lua", ""));
+                runAll(file.GetSetting(element, "onChecked.lua", ""), file.GetSetting(element, "onChecked.cmd", ""));
             }
             else
             {
-                runCmd(Environment.CurrentDirectory + "\\lua.exe " + file.GetSetting(element, "onUnchecked.lua", ""));
+                runAll(file.GetSetting(element, "onUnchecked.lua", ""), file.GetSetting(element, "onUnchecked.cmd", ""));
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             IniFile file = new IniFile();
-            file.Load(".\\main.ini");
+            file.Load(Properties.Settings.Default.Default_Ini_Filepath);
 
+            MessageBox.Show(" ", "luna - Error", MessageBoxButtons.YesNo, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RtlReading);
             Text = file.GetSetting(IniFile.DefaultSectionName, "window.title", "Window");
             Width = file.GetSetting(IniFile.DefaultSectionName, "window.sz.x", 500);
             Height = file.GetSetting(IniFile.DefaultSectionName, "window.sz.y", 500);
@@ -82,7 +102,7 @@ namespace inui
                     button.Location = new Point(file.GetSetting(element, "pos.x", 10), file.GetSetting(element, "pos.y", 10));
                     button.Size = new Size(file.GetSetting(element, "sz.x", 75), file.GetSetting(element, "sz.y", 23));
 
-                    button.Click += (_, __) => runCmd(Environment.CurrentDirectory + "\\lua.exe " + file.GetSetting(element, "onClicked.lua", ""));
+                    button.Click += (_, __) => runAll(file.GetSetting(element, "onClicked.lua", ""), file.GetSetting(element, "onClicked.cmd", ""));
 
                     controls_string.Add(element.Split(':')[1]);
                     controls_object.Add(button);
@@ -100,8 +120,14 @@ namespace inui
                         label.Parent = controls_object[controls_string.IndexOf(file.GetSetting(element, "parent", ""))];
                     }
 
+                    if (Properties.Settings.Default.Custom_Label_Size)
+                    {
+                        label.Size = new Size(file.GetSetting(element, "sz.x", 50), file.GetSetting(element, "sz.y", 10));
+                    }
+
                     label.Text = file.GetSetting(element, "text", "Label");
                     label.Location = new Point(file.GetSetting(element, "pos.x", 10), file.GetSetting(element, "pos.y", 10));
+                    label.Size = new Size(label.Text.Length * 5, label.Height);
 
                     controls_string.Add(element.Split(':')[1]);
                     controls_object.Add(label);
@@ -140,8 +166,8 @@ namespace inui
 
                     button.Text = file.GetSetting(element, "text", "RadioButton");
                     button.Location = new Point(file.GetSetting(element, "pos.x", 10), file.GetSetting(element, "pos.y", 10));
-
-                    button.Click += (_, __) => runCmd(Environment.CurrentDirectory + "\\lua.exe " + file.GetSetting(element, "onClicked.lua", "")); // this was a pain at first
+                            
+                    button.Click += (_, __) => runAll(file.GetSetting(element, "onClicked.lua", ""), file.GetSetting(element, "onClicked.cmd", "")); // this was a pain at first
 
                     controls_string.Add(element.Split(':')[1]);
                     controls_object.Add(button);
@@ -170,25 +196,84 @@ namespace inui
 
                     pic.BackgroundImage = Image.FromFile(file.GetSetting(element, "path", "")); // needs to be background image because that has sizing
 
-                    if (file.GetSetting(element, "sizing", "zoom") == "tile") // sizing because getting the size perfectly right wth sz.x/y is a pain
+                    if (file.GetSetting(element, "sizing", Properties.Settings.Default.Default_Image_Sizing_Mode) == "tile") // sizing because getting the size perfectly right wth sz.x/y is a pain
                     {
                         pic.BackgroundImageLayout = ImageLayout.Tile;
                     }
-                    else if (file.GetSetting(element, "sizing", "zoom") == "stretch")
+                    else if (file.GetSetting(element, "sizing", Properties.Settings.Default.Default_Image_Sizing_Mode) == "stretch")
                     {
                         pic.BackgroundImageLayout = ImageLayout.Stretch;
                     }
-                    else if (file.GetSetting(element, "sizing", "zoom") == "zoom")
+                    else if (file.GetSetting(element, "sizing", Properties.Settings.Default.Default_Image_Sizing_Mode) == "zoom")
                     {
                         pic.BackgroundImageLayout = ImageLayout.Zoom;
                     }
-                    else if (file.GetSetting(element, "sizing", "zoom") == "center")
+                    else if (file.GetSetting(element, "sizing", Properties.Settings.Default.Default_Image_Sizing_Mode) == "center")
                     {
                         pic.BackgroundImageLayout = ImageLayout.Zoom;
                     }
 
                     controls_string.Add(element.Split(':')[1]);
                     controls_object.Add(pic);
+                }
+                if (element.Split(':')[0] == "ListBox")
+                {
+                    ListBox list = new ListBox();
+                    list.Name = element.Split(':')[1];
+                    if (file.GetSetting(element, "parent", "") == "")
+                    {
+                        list.Parent = this;
+                    }
+                    else
+                    {
+                        list.Parent = controls_object[controls_string.IndexOf(file.GetSetting(element, "parent", ""))];
+                    }
+
+                    list.Location = new Point(file.GetSetting(element, "pos.x", 10), file.GetSetting(element, "pos.y", 10));
+                    list.Size = new Size(file.GetSetting(element, "sz.x", 100), file.GetSetting(element, "sz.y", 100));
+
+                    string[] strings = file.GetSetting(element, "items", "Item 1,Item 2").Split(',');
+
+                    foreach (string s in strings)
+                    {
+                        list.Items.Add(s);
+                    }
+
+                    list.SelectedIndexChanged += (_, __) => runAll(file.GetSetting(element, "onSelected.lua", ""), file.GetSetting(element, "onSelected.cmd", "")); // idk how im gonna transfer vals to the lua side
+
+                    controls_string.Add(element.Split(':')[1]);
+                    controls_object.Add(list);
+                }
+                if (element.Split(':')[0] == "TabControl")
+                {
+                    TabControl tabs = new TabControl();
+                    tabs.Name = element.Split(':')[1];
+
+                    if (file.GetSetting(element, "parent", "") == "")
+                    {
+                        tabs.Parent = this;
+                    }
+                    else
+                    {
+                        tabs.Parent = controls_object[controls_string.IndexOf(file.GetSetting(element, "parent", ""))];
+                    }
+
+                    tabs.Location = new Point(file.GetSetting(element, "pos.x", 10), file.GetSetting(element, "pos.y", 10));
+                    tabs.Size = new Size(file.GetSetting(element, "sz.x", 100), file.GetSetting(element, "sz.y", 100));
+
+                    string[] strings = file.GetSetting(element, "tabs", "Tab 1,Tab 2").Split(',');
+
+                    foreach (string s in strings)
+                    {
+                        TabPage tab = new TabPage();
+                        tab.Name = s;
+                        tab.Text = s;
+
+                        tabs.TabPages.Add(tab);
+
+                        controls_string.Add(element.Split(':')[1] + "." + s);
+                        controls_object.Add(tab);
+                    }
                 }
             }
         }
--- inui/Properties/Settings.Designer.cs
+++ inui/Properties/Settings.Designer.cs
@@ -8,22 +8,54 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace inui.Properties
-{
-
-
+namespace inui.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Custom_Label_Size {
+            get {
+                return ((bool)(this["Custom_Label_Size"]));
+            }
+            set {
+                this["Custom_Label_Size"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("zoom")]
+        public string Default_Image_Sizing_Mode {
+            get {
+                return ((string)(this["Default_Image_Sizing_Mode"]));
+            }
+            set {
+                this["Default_Image_Sizing_Mode"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute(".\\main.ini")]
+        public string Default_Ini_Filepath {
+            get {
+                return ((string)(this["Default_Ini_Filepath"]));
+            }
+            set {
+                this["Default_Ini_Filepath"] = value;
             }
         }
     }
